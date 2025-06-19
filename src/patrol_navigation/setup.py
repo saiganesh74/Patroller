1172e12a64edcbb69c655e7284cd1727568f1073
saiganesh74 <ganeshrejeti1@gmail.com>
2025-06-19 07:47:19 +0530

    setup file is updated

--- src/patrol_navigation/setup.py
+++ src/patrol_navigation/setup.py
@@ -1,25 +1,20 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'patrol_navigation'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    version='0.1.0',
+    packages=[package_name],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='saiganesh',
+    maintainer='Sai Ganesh',
     maintainer_email='ganeshrejeti1@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    description='Handles patrol path logic',
+    license='MIT',
     entry_points={
         'console_scripts': [
+            'patrol_node = patrol_navigation.patrol_node:main',
         ],
     },
 )
