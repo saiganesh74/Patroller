602361b934cb2bb6e7d421090cab7eff28f2c1e2
saiganesh74 <ganeshrejeti1@gmail.com>
2025-06-20 18:24:56 +0530

    Setup file

--- src/incident_logger/setup.py
+++ src/incident_logger/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'incident_logger = incident_logger.incident_logger:main',
         ],
     },
 )
